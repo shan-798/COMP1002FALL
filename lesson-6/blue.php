4f50575b9971f55f577553568d7779bd412342fa
shan-798 <shan.palomino@gmail.com>
2025-10-10 16:41:41 +0000

    created nsv php

--- lesson-6/blue.php
+++ lesson-6/blue.php
@@ -15,14 +15,10 @@
 			<h1>HTML, CSS, and JS Fundamentals</h1>
 			<h2>Building Blocks for Web Developers</h2>
 			<!-- Global site navigation -->
-			<nav>
-				<ul>
-					<li><a href="index.php" title="Go to home page">Home</a></li>
-					<li><a href="red.php" title="All about red blocks">Red blocks</a></li>
-					<li><a href="blue.php" title="All about blue blocks">Blue blocks</a></li>
-					<li><a href="yellow.php" title="All about yellow blocks">Yellow blocks</a></li>
-				</ul>
-			</nav>
+			<?php
+				// Grab site navigation
+				include("includes/nav.php");
+			?>
 		</header>
 		<!-- Page-level main content -->
 		<main>
---
+++ lesson-6/includes/nav.php
@@ -0,0 +1,8 @@
+			<nav>
+				<ul>
+					<li><a href="/lesson-6/index.php" title="Go to home page">Home</a></li>
+					<li><a href="/lesson-6/red.php" title="All about red blocks">Red blocks</a></li>
+					<li><a href="/lesson-6/blue.php" title="All about blue blocks">Blue blocks</a></li>
+					<li><a href="/lesson-6/yellow.php" title="All about yellow blocks">Yellow blocks</a></li>
+				</ul>
+			</nav>
