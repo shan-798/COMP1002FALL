c252d42a89e11174de32adb53a65f1547f18f739
Shana Palomino <shan.palomino@gmail.com>
2025-11-02 11:07:37 -0500

    added form code + created .php file

---
+++ assignment-1/pizza-form.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Order Complete</title>
+</head>
+<body>
+        <header>
+        <h1>Your order has been processed!</h1>
+    </header>
+    <main>
+        <p> Thank you for ordering, your pizza will be here shortly.
+</main>
+    <footer>
+        <br>
+        <small>©PizzaDude USA. All rights reserved.</small>
+    </footer>
+</body>
+</html>
